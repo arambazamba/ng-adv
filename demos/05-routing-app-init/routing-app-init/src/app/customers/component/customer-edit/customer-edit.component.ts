85180ed0412b76d461f984d88764c42665ebd5e6
Alexander Pajer <alexander.pajer@integrations.at>
2023-04-25 08:00:26 +0200

    bindToComponentInputs

--- demos/05-routing-app-init/routing-app-init/src/app/customers/component/customer-edit/customer-edit.component.ts
+++ demos/05-routing-app-init/routing-app-init/src/app/customers/component/customer-edit/customer-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { State } from '@ngrx/store';
+import { CustomersState } from '../../state/customers.reducer';
 
 @Component({
   selector: 'app-customer-edit',
@@ -7,8 +9,10 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CustomerEditComponent implements OnInit {
   @Input() id?: string;
+  @Input() readonly?: boolean;
+  state = inject(State<CustomersState>);
 
   ngOnInit(): void {
-    console.log('CustomerEditComponent - ngOnInit', this.id);
+
   }
 }
