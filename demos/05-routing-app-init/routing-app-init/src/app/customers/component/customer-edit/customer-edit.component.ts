8fe7d2e62dd8fe1916d2609abd2caa021b64ac86
Alexander Pajer <alexander.pajer@integrations.at>
2023-04-25 08:22:21 +0200

    Fixes #228

--- demos/05-routing-app-init/routing-app-init/src/app/customers/component/customer-edit/customer-edit.component.ts
+++ demos/05-routing-app-init/routing-app-init/src/app/customers/component/customer-edit/customer-edit.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
-import { State } from '@ngrx/store';
+import { Component, Input, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { mergeMap } from 'rxjs';
 import { CustomersState } from '../../state/customers.reducer';
+import { getCustomers } from '../../state/customers.selector';
 
 @Component({
   selector: 'app-customer-edit',
   templateUrl: './customer-edit.component.html',
   styleUrls: ['./customer-edit.component.scss']
 })
-export class CustomerEditComponent implements OnInit {
-  @Input() id?: string;
+export class CustomerEditComponent {
+  @Input() id?: number;
   @Input() readonly?: boolean;
-  state = inject(State<CustomersState>);
-
-  ngOnInit(): void {
-
-  }
+  store = inject(Store<CustomersState>) as Store<CustomersState>;
+  customer = this.store.select(getCustomers).pipe(
+    mergeMap(
+      (customers) => customers.filter(c => c.id == this.id)
+    ));
 }
