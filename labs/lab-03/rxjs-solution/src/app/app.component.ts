adac6f550e45460314f49b49097c7689b4d928a6
Alexander Pajer <alexander.pajer@integrations.at>
2023-08-14 22:04:44 +0200

    l3 scaffold

--- labs/lab-03/rxjs-solution/src/app/app.component.ts
+++ labs/lab-03/rxjs-solution/src/app/app.component.ts
@@ -1,38 +1,15 @@
-import { Component, inject, ChangeDetectorRef, AfterContentChecked } from '@angular/core';
-import { MatDrawerMode } from '@angular/material/sidenav';
-import { MenuService } from './shared/menu/menu.service';
-import { LoadingService } from './shared/loading/loading.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [NavbarComponent, SidebarComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'Food App';
-  mode: MatDrawerMode = 'side';
-  ms = inject(MenuService);
-  ls = inject(LoadingService);
-  changeDetector = inject(ChangeDetectorRef);
-  isLoading = this.ls.getLoading();
-
-  constructor() {
-    this.ms.sideNavPosition.subscribe((currentMode) => { this.mode = currentMode });
-  }
-
-  ngAfterContentChecked(): void {
-    this.changeDetector.detectChanges();
-  }
-
-  getWorbenchStyle() {
-    let result = {};
-    this.ms.sideNavVisible.subscribe((visible) => {
-      result = visible
-        ? {
-          'padding-left': '10px',
-        }
-        : {};
-    });
-    return result;
-  }
 }
--- labs/lab-03/rxjs-solution/src/app/shared/sidemenu/sidemenu.component.ts
+++ labs/lab-03/rxjs-solution/src/app/shared/sidemenu/sidemenu.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styleUrls: ['./sidemenu.component.scss']
+  styleUrls: ['./sidemenu.component.scss'],
+  standalone: true
 })
-export class SidemenuComponent {
-}
+export class SidemenuComponent { }
