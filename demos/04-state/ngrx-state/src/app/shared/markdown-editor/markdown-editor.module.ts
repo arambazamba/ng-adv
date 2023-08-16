758ad8547715d7bc5be69385d6399021153d8145
Alexander Pajer <alexander.pajer@integrations.at>
2023-08-16 08:35:08 +0200

    docs

--- demos/04-state/ngrx-state/src/app/shared/markdown-editor/markdown-editor.module.ts
+++ demos/04-state/ngrx-state/src/app/shared/markdown-editor/markdown-editor.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { MaterialModule } from '../../material.module';
-import { EditorEffects } from './state/editor.effects';
-import { EditorContainerComponent } from './components/editor-container/editor-container.component';
-import { CommentsListComponent } from './components/comments-list/comments-list.component';
-import { CommentEditComponent } from './components/comment-edit/comment-edit.component';
 import { ColumnDirective, RowDirective } from '../formatting/formatting-directives';
+import { CommentEditComponent } from './components/comment-edit/comment-edit.component';
+import { CommentsListComponent } from './components/comments-list/comments-list.component';
+import { EditorContainerComponent } from './components/editor-container/editor-container.component';
+import { EditorEffects } from './state/editor.effects';
 import { editorState } from './state/editor.state';
 
 const comps = [
