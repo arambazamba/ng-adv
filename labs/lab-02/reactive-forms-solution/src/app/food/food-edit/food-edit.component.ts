99152847bcb6a39ae2dce2ec63cd3dca1bf83f65
Alexander Pajer <alexander.pajer@integrations.at>
2023-09-12 11:57:03 +0200

    inject

--- labs/lab-02/reactive-forms-solution/src/app/food/food-edit/food-edit.component.ts
+++ labs/lab-02/reactive-forms-solution/src/app/food/food-edit/food-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FoodItem } from '../food.model';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,26 +8,27 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
-    selector: 'app-food-edit',
-    templateUrl: './food-edit.component.html',
-    styleUrls: ['./food-edit.component.scss'],
-    standalone: true,
-    imports: [
-        MatCardModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        NgIf,
-        MatButtonModule,
-    ],
+  selector: 'app-food-edit',
+  templateUrl: './food-edit.component.html',
+  styleUrls: ['./food-edit.component.scss'],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NgIf,
+    MatButtonModule,
+  ],
 })
 export class FoodEditComponent implements OnChanges {
+  fb = inject(FormBuilder);
   @Input() food: FoodItem = new FoodItem();
   @Output() onFoodSaved: EventEmitter<FoodItem> = new EventEmitter<FoodItem>();
   foodForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor() {
     this.foodForm = this.fb.group({
       id: [this.food.id],
       name: [this.food.name, [Validators.required, Validators.minLength(3)]],
