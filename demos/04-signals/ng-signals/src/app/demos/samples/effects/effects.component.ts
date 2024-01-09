399d208621cb0af4a0fbc08ccbd718a4a3948ad1
Alexander Pajer <alexander.pajer@integrations.at>
2024-01-09 16:46:27 +0100

    signals

---
+++ demos/04-signals/ng-signals/src/app/demos/samples/effects/effects.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-effects',
+  templateUrl: './effects.component.html',
+  styleUrls: ['./effects.component.scss']
+})
+export class EffectsComponent { }
