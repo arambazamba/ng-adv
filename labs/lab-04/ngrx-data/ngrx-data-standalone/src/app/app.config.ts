9b7de905c07107db019a8ba46437ddc45c1d892f
Alexander Pajer <alexander.pajer@integrations.at>
2024-01-21 09:21:54 +0100

    base

--- labs/lab-04/ngrx-data/ngrx-data-standalone/src/app/app.config.ts
+++ labs/lab-04/ngrx-data/ngrx-data-standalone/src/app/app.config.ts
@@ -1,21 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideEntityData, withEffects } from '@ngrx/data';
-import { provideEffects } from '@ngrx/effects';
-import { provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
+
 import { routes } from './app.routes';
-import { entityConfig } from './entity-metadata';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideHttpClient(),
-    provideRouter(routes),
-    provideStore(),
-    provideEffects(),
-    provideStoreDevtools(),
-
-    // provideEntityData(entityConfig, withEffects())
-  ]
+  providers: [provideRouter(routes)]
 };
