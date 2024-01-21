86cdced0e96165cb16a25bec4fade82102434cb9
Alexander Pajer <alexander.pajer@integrations.at>
2024-01-21 09:27:25 +0100

    scaffold

--- labs/lab-04/ngrx-data/ngrx-data-standalone/src/app/app.config.ts
+++ labs/lab-04/ngrx-data/ngrx-data-standalone/src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools(),
+  ]
 };
