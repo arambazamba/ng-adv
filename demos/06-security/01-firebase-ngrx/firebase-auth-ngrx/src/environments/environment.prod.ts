8a848ac2e7565ed2b427a394e6a846fa9dd90ae2
Alexander Pajer <alexander.pajer@integrations.at>
2024-01-22 20:47:07 +0100

    auth demo

--- demos/06-security/01-firebase-ngrx/firebase-auth-ngrx/src/environments/environment.prod.ts
+++ demos/06-security/01-firebase-ngrx/firebase-auth-ngrx/src/environments/environment.prod.ts
@@ -4,16 +4,16 @@ export const environment = {
   title: 'Authentication',
   markdownPath: '/assets/markdown/',
   api: 'http://localhost:3000/',
-  netapi: 'https://localhost:5001/',
+  netApi: 'https://localhost:5001/',
   logChangeDetection: false,
   logChanges: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBDZlA5Inq9ngFIyxj0L_--ojZbNGbyNqo",
-    authDomain: "ng-adv-3156c.firebaseapp.com",
-    projectId: "ng-adv-3156c",
-    storageBucket: "ng-adv-3156c.appspot.com",
-    messagingSenderId: "917374484498",
-    appId: "1:917374484498:web:b4fa1e7ceb52f186f858cb",
-    measurementId: "G-GECR93RSTR"
+    apiKey: "AIzaSyDhQ_ZFwP2QaoNsQssxGq9eoECR42QrF48",
+    authDomain: "food-app-ap.firebaseapp.com",
+    projectId: "food-app-ap",
+    storageBucket: "food-app-ap.appspot.com",
+    messagingSenderId: "581454033222",
+    appId: "1:581454033222:web:3458f3753fcdcdca6ff547",
+    measurementId: "G-TSCT3YVZLE"
   },
 };
