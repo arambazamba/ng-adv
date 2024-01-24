31c07827f8accdc8d00521efece082ee1e64d161
Alexander Pajer <alexander.pajer@integrations.at>
2024-01-24 19:39:03 +0100

    auth working

--- labs/lab-07/auth-solution/src/app/app.component.ts
+++ labs/lab-07/auth-solution/src/app/app.component.ts
@@ -5,16 +5,24 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { tap } from 'rxjs';
 import { FirebaseAuthService } from './firebase-auth/firebase-auth.service';
 import { IntroComponent } from './shared/intro/intro.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, IntroComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    SidebarComponent,
+    IntroComponent,
+    AsyncPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'food-app';
+  title = 'Food App';
+  img = 'food.png'
   auth = inject(FirebaseAuthService);
   isAuthenticated = this.auth
     .isAuthenticated()
