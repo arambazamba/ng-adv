f5496487103004d076d3d625d2b53a927ebc81c6
Alexander Kastil <alexander.kastil@integrations.at>
2024-05-15 10:55:06 +0200

    fix: testing

--- demos/07-testing/01-jasmine/jasmine-testing/src/app/demos/samples/ngrx-mockstore/mockstore/mockstore.component.spec.ts
+++ demos/07-testing/01-jasmine/jasmine-testing/src/app/demos/samples/ngrx-mockstore/mockstore/mockstore.component.spec.ts
@@ -1,9 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { DemoState } from '../../../state/demos.reducer';
 import { MockstoreComponent } from './mockstore.component';
 import { mockstoreData } from './mockstore.data';
+import { DemoState } from 'src/app/demos/state/demos.state';
 
 
 describe('MockstoreComponent', () => {
@@ -14,9 +14,9 @@ describe('MockstoreComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [MockstoreComponent],
-    providers: [provideMockStore({ initialState })]
-}).compileComponents();
+      imports: [MockstoreComponent],
+      providers: [provideMockStore({ initialState })]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MockstoreComponent);
     component = fixture.componentInstance;
