868fc01c2c06df3097d1be1a900997c1ea58c1a1
Alexander Kastil <alexander.kastil@integrations.at>
2024-05-15 10:47:12 +0200

    fix: facades component

--- demos/03-state-ngrx/ngrx-state/src/app/demos/samples/facades/facades.component.ts
+++ demos/03-state-ngrx/ngrx-state/src/app/demos/samples/facades/facades.component.ts
@@ -6,28 +6,29 @@ import { AsyncPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardActions } from '@angular/material/card';
 import { MarkdownRendererComponent } from '../../../shared/markdown-renderer/markdown-renderer.component';
+import { AuthFacade } from 'src/app/state/auth.facade';
 
 @Component({
-    selector: 'app-facades',
-    templateUrl: './facades.component.html',
-    styleUrls: ['./facades.component.scss'],
-    standalone: true,
-    imports: [
-        MarkdownRendererComponent,
-        MatCard,
-        MatCardHeader,
-        MatCardTitle,
-        MatCardContent,
-        MatCardActions,
-        MatButton,
-        AsyncPipe,
-    ],
+  selector: 'app-facades',
+  templateUrl: './facades.component.html',
+  styleUrls: ['./facades.component.scss'],
+  standalone: true,
+  imports: [
+    MarkdownRendererComponent,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    AsyncPipe,
+  ],
 })
 export class FacadesComponent {
-  store = inject(Store<AppState>);
-  isMockAuthenticated = this.store.select(appState.selectIsMockAuthenticated);
+  auth = inject(AuthFacade);
+  isMockAuthenticated = this.auth.getIsAuth();
 
   toggleAuth() {
-    this.store.dispatch(appActions.toggleMockAuthenticated());
+    this.auth.toggleAuth();
   }
 }
